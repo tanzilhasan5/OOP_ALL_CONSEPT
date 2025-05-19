e67f1d0bb15fa045ff3d2cc47bb4e76994259488
Tanzil Hasan <tanzilhasan151@gmail.com>
2025-05-19 09:49:28 +0600

    Update inheritance.dart

--- lib/oop/inheritance/inheritance.dart
+++ lib/oop/inheritance/inheritance.dart
@@ -1,6 +1,7 @@
 class  Person{
   String university ='Dhaka Univeraity';
   String _univeraity = 'Dhaka University';
+/* private class ke onno file thake Access korar jnno ai method use kora hoy*/
   String getPersonDetails(){
     return 'person details';
   }
