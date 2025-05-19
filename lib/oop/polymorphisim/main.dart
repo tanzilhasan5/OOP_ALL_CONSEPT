a207ef202995929f92fc5218e6994e6f82f24348
Tanzil Hasan <tanzilhasan151@gmail.com>
2025-05-19 09:43:53 +0600

    Update main.dart

--- lib/oop/polymorphisim/main.dart
+++ lib/oop/polymorphisim/main.dart
@@ -1,10 +1,12 @@
 import 'package:dartoop/oop/polymorphisim/phylimorphsim.dart';
 
 void main(){
+// implement human class
   Human safa =Human();
   Human rafu = Studetn();
   Human tanzil = SoftwerEngnner();
   safa.eating();
+/* jodi safa software engineers hoy tahole software engineers behaviour safa pabe*/
   if (safa is SoftwerEngnner){
     safa.coding();
   }
